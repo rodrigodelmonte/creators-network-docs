09dc0161065bb2175a4748a3a3522e098c4b193e
dissroff <39369649+dissroff@users.noreply.github.com>
2020-07-09 12:01:41 +0200

    Update favicon (#6)
    * change url
    
    * add favicon

--- source/conf.py
+++ source/conf.py
@@ -27,6 +27,7 @@ exclude_patterns = []
 
 
 html_theme = 'flask'
+html_favicon = '_static/favicon.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
